0a8ad0693c88c393d5531f647fc560740363c0ee
Maximilian Krauß <max@krauss.io>
2024-09-16 11:39:58 +0200

    chore: Improves install and update script

--- install.sh
+++ install.sh
@@ -10,13 +10,14 @@ set -eo pipefail
 
 UNAME=$(echo uname)
 ARCH=$(arch)
-LATEST_VERSION=$(curl -s https://api.github.com/repos/maximilian-krauss/roehrich/releases/latest | jq -r '.tag_name')
-DOWNLOAD_URL="https://github.com/maximilian-krauss/roehrich/releases/download/$LATEST_VERSION/roehrich_$(UNAME)_${ARCH}.tar.gz"
-DOWNLOAD_DESTINATION="/tmp/roehrich_$LATEST_VERSION.tar.gz"
+NAME=roehrich
+LATEST_VERSION=$(curl -s "https://api.github.com/repos/maximilian-krauss/$NAME/releases/latest" | jq -r '.tag_name')
+DOWNLOAD_URL="https://github.com/maximilian-krauss/$NAME/releases/download/$LATEST_VERSION/${NAME}_$(UNAME)_${ARCH}.tar.gz"
+DOWNLOAD_DESTINATION="/tmp/${NAME}_$LATEST_VERSION.tar.gz"
 
 curl -L -s -o "$DOWNLOAD_DESTINATION" "$DOWNLOAD_URL"
 tar -xzvf "$DOWNLOAD_DESTINATION" -C /tmp
 
-chmod +x /tmp/roehrich
-sudo mv -f "/tmp/roehrich" "/usr/local/bin"
+chmod +x "/tmp/$NAME"
+sudo mv -f "/tmp/$NAME" "/usr/local/bin"
 rm "$DOWNLOAD_DESTINATION"
