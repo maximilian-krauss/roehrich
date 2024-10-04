d875a2c943f1ed42e7f8b1cce15c143b7c1afb52
Maximilian Krauß <max@krauss.io>
2024-10-04 15:53:14 +0200

    refactor: Moves actual logic into its own package

--- cmd/root.go
+++ cmd/root.go
@@ -1,37 +1,15 @@
 package cmd
 
 import (
-	"log"
-	"time"
-
-	"github.com/maximilian-krauss/roehrich/config"
-	"github.com/maximilian-krauss/roehrich/gitlab"
 	"github.com/maximilian-krauss/roehrich/input"
-	"github.com/maximilian-krauss/roehrich/utils"
+	"github.com/maximilian-krauss/roehrich/statuscheck"
 	"github.com/spf13/cobra"
+	"log"
 )
 
 func onlyUrls(_ *cobra.Command, args []string) error {
 	maybeUrl := args[0]
 	return input.ValidateUrl(maybeUrl)
-}
-
-func printGroupedJobs(jobs []gitlab.Job) {
-	jobsGroupedByStage := utils.GroupByProperty(jobs, func(j gitlab.Job) string {
-		return j.Stage
-	})
-
-	for stage, group := range jobsGroupedByStage {
-		log.Printf("=== %s ===", stage)
-		for _, job := range group {
-			printJob(job)
-		}
-	}
-}
-
-func printJob(job gitlab.Job) {
-	statusColor := utils.JobStatusToColor(job.Status)
-	log.Printf("%s  %s\n", statusColor.SprintFunc()("["+job.Status+"]"), job.Name)
 }
 
 var rootCmd = &cobra.Command{
@@ -43,87 +21,15 @@ var rootCmd = &cobra.Command{
 		onlyUrls,
 		cobra.OnlyValidArgs,
 	),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		cfg, err := config.LoadConfig()
+	Run: func(cmd *cobra.Command, args []string) {
+		source := args[0]
+		err := statuscheck.Run(statuscheck.Args{
+			SourceUrl:                source,
+			PollingIntervalInSeconds: 10,
+		})
 		if err != nil {
-			return err
+			log.Fatal(err)
 		}
-		mrInfo, err := input.GetMRInfo(args[0])
-		if err != nil {
-			return err
-		} else {
-			log.Printf("Found project name %s and merge request id %s", mrInfo.ProjectName, mrInfo.Id)
-		}
-
-		gitlabConfig, err := config.GetConfigByHostname(mrInfo.HostName, *cfg)
-		if err != nil {
-			return err
-		}
-
-		if err := gitlab.CheckToken(*gitlabConfig); err != nil {
-			return err
-		} else {
-			log.Println("access token verified")
-		}
-
-		mergeRequest, err := gitlab.GetMergeRequest(mrInfo, *gitlabConfig)
-		if err != nil {
-			return err
-		} else {
-			log.Printf("resolved merge request: %s\n", mergeRequest.Title)
-		}
-
-		if mergeRequest.State != "opened" {
-			log.Printf("merge request is already %s", mergeRequest.State)
-			return nil
-		} else {
-			log.Printf("merge request is in valid state: %s\n", mergeRequest.State)
-		}
-
-		if mergeRequest.Pipeline.Status == "success" {
-			log.Printf("%s pipeline did already succeed", "✅")
-			return nil
-		}
-
-		jobs, err := gitlab.GetJobs(mergeRequest, *gitlabConfig, nil)
-		if err != nil {
-			return err
-		}
-		printGroupedJobs(jobs)
-
-		finishedJobs := make(map[int]gitlab.Job)
-		for _, job := range gitlab.FilterFinishedJobs(jobs) {
-			finishedJobs[job.Id] = job
-		}
-
-		log.Println("waiting for other jobs to complete...")
-
-		for {
-			jobs, err := gitlab.GetJobs(mergeRequest, *gitlabConfig, gitlab.FinishedJobStatuses)
-			if err != nil {
-				return err
-			}
-			for _, job := range jobs {
-				if _, exists := finishedJobs[job.Id]; exists {
-					continue
-				}
-				finishedJobs[job.Id] = job
-				printJob(job)
-			}
-
-			pipeline, err := gitlab.GetPipeline(mergeRequest, *gitlabConfig)
-			if err != nil {
-				return err
-			}
-			if !pipeline.IsPendingOrRunning {
-				log.Printf("pipeline changed status to %s", pipeline.Status)
-				break
-			}
-
-			time.Sleep(10 * time.Second)
-		}
-
-		return nil
 	},
 }
 
---
+++ statuscheck/run.go
@@ -0,0 +1,114 @@
+package statuscheck
+
+import (
+	"github.com/maximilian-krauss/roehrich/config"
+	"github.com/maximilian-krauss/roehrich/gitlab"
+	"github.com/maximilian-krauss/roehrich/input"
+	"github.com/maximilian-krauss/roehrich/utils"
+	"log"
+	"time"
+)
+
+type Args struct {
+	SourceUrl                string
+	PollingIntervalInSeconds int
+}
+
+func printGroupedJobs(jobs []gitlab.Job) {
+	jobsGroupedByStage := utils.GroupByProperty(jobs, func(j gitlab.Job) string {
+		return j.Stage
+	})
+
+	for stage, group := range jobsGroupedByStage {
+		log.Printf("=== %s ===", stage)
+		for _, job := range group {
+			printJob(job)
+		}
+	}
+}
+
+func printJob(job gitlab.Job) {
+	statusColor := utils.JobStatusToColor(job.Status)
+	log.Printf("%s  %s\n", statusColor.SprintFunc()("["+job.Status+"]"), job.Name)
+}
+
+func Run(args Args) error {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		return err
+	}
+	mrInfo, err := input.GetMRInfo(args.SourceUrl)
+	if err != nil {
+		return err
+	}
+	log.Printf("Found project name %s and merge request id %s", mrInfo.ProjectName, mrInfo.Id)
+
+	gitlabConfig, err := config.GetConfigByHostname(mrInfo.HostName, *cfg)
+	if err != nil {
+		return err
+	}
+
+	if err := gitlab.CheckToken(*gitlabConfig); err != nil {
+		return err
+	}
+	log.Println("access token verified")
+
+	mergeRequest, err := gitlab.GetMergeRequest(mrInfo, *gitlabConfig)
+	if err != nil {
+		return err
+	}
+	log.Printf("resolved merge request: %s\n", mergeRequest.Title)
+
+	if mergeRequest.State != "opened" {
+		log.Printf("merge request is already %s", mergeRequest.State)
+		return nil
+	} else {
+		log.Printf("merge request is in valid state: %s\n", mergeRequest.State)
+	}
+
+	if mergeRequest.Pipeline.Status == "success" {
+		log.Println("pipeline did already succeed")
+		return nil
+	}
+
+	jobs, err := gitlab.GetJobs(mergeRequest, *gitlabConfig, nil)
+	if err != nil {
+		return err
+	}
+	printGroupedJobs(jobs)
+
+	finishedJobs := make(map[int]gitlab.Job)
+	for _, job := range gitlab.FilterFinishedJobs(jobs) {
+		finishedJobs[job.Id] = job
+	}
+
+	log.Println("waiting for other jobs to complete...")
+
+	for {
+		jobs, err := gitlab.GetJobs(mergeRequest, *gitlabConfig, gitlab.FinishedJobStatuses)
+		if err != nil {
+			return err
+		}
+		for _, job := range jobs {
+			if _, exists := finishedJobs[job.Id]; exists {
+				continue
+			}
+			finishedJobs[job.Id] = job
+			printJob(job)
+		}
+
+		pipeline, err := gitlab.GetPipeline(mergeRequest, *gitlabConfig)
+		if err != nil {
+			return err
+		}
+		if !pipeline.IsPendingOrRunning {
+			statusColor := utils.JobStatusToColor(pipeline.Status)
+			log.Printf("pipeline changed status to %s", statusColor.SprintFunc()(pipeline.Status))
+			break
+		}
+
+		time.Sleep(time.Second * time.Duration(args.PollingIntervalInSeconds))
+	}
+
+	return nil
+}
