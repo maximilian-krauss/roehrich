b590fa5758db46c1698d765c0d4059e7a34d27d6
Maximilian Krauss <max@krauss.io>
2024-12-05 08:25:10 +0100

    ✨ feat: Adds version check

--- cmd/version.go
+++ cmd/version.go
@@ -2,11 +2,22 @@ package cmd
 
 import (
 	"fmt"
+
+	"github.com/maximilian-krauss/roehrich/update"
 )
 
 const ApplicationVersion = "0.0.11"
 
 func runVersion() error {
-	fmt.Printf("Current version: %s\n", ApplicationVersion)
+	fmt.Printf("current version: %s\n", ApplicationVersion)
+
+	remoteVersion, err := update.FindLatestVersion(ApplicationVersion)
+	if err != nil {
+		fmt.Printf("failed to check for latest release version: %s", err.Error())
+	} else if remoteVersion.IsNewer {
+		fmt.Printf("latest version: %s (%s)", remoteVersion.Version, remoteVersion.Url)
+		fmt.Println()
+	}
+
 	return nil
 }
---
+++ update/update.go
@@ -0,0 +1,83 @@
+package update
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/hashicorp/go-version"
+)
+
+const RELEASE_URL = "https://api.github.com/repos/maximilian-krauss/roehrich/releases/latest"
+
+type GithubRelease struct {
+	TagName string `json:"tag_name"`
+	Url     string `json:"html_url"`
+}
+
+func getLatestGithubRelease() (*GithubRelease, error) {
+	httpClient := http.Client{}
+	requestUri, err := url.ParseRequestURI(RELEASE_URL)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest("GET", requestUri.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header = http.Header{
+		"Content-Type": []string{"application/json"},
+		"Accept":       []string{"application/json"},
+	}
+
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request to %s failed with status code %d", RELEASE_URL, response.StatusCode)
+	}
+
+	var githubRelease GithubRelease
+	if err := json.Unmarshal(body, &githubRelease); err != nil {
+		return nil, err
+	}
+	return &githubRelease, nil
+}
+
+type VersionInfo struct {
+	Version string
+	IsNewer bool
+	Url     string
+}
+
+func FindLatestVersion(currentVersionString string) (*VersionInfo, error) {
+	latestRelease, err := getLatestGithubRelease()
+	if err != nil {
+		return nil, nil
+	}
+
+	currentVersion, err := version.NewVersion(currentVersionString)
+	if err != nil {
+		return nil, err
+	}
+	releaseVersion, err := version.NewVersion(latestRelease.TagName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &VersionInfo{
+		Version: latestRelease.TagName,
+		IsNewer: releaseVersion.GreaterThan(currentVersion),
+		Url:     latestRelease.Url,
+	}, nil
+}
